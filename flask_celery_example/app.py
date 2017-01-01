b2e8eb7a40fab497dcb99df605682f0d6ddfdece
Zed <ninpuabi@gmail.com>
2017-01-01 11:09:56 +0000

    celery

--- flask_celery_example/app.py
+++ flask_celery_example/app.py
@@ -9,7 +9,7 @@ import logging
 from logging.handlers import RotatingFileHandler
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'top-secret!'
-from tasks import *
+
 
 # Flask-Mail configuration
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
@@ -96,7 +96,7 @@ def longtask():
 @app.route('/status/<task_id>')
 def taskstatus(task_id):
     task = long_task.AsyncResult(task_id)
-    app.logger.debug("task status. {0}".format(task.state))
+    app.logger.debug("task status. {0} {1}".format(task.state, task_id))
     if task.state == 'PENDING':
         response = {
             'state': task.state,
